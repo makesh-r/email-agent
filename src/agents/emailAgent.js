b8135486d2774de76e24c7d2148caa9f10846f30
Makesh R <makesh@5xtechnologies.in>
2025-10-07 07:55:33 +0530

    removed date formatter agent

--- src/agents/emailAgent.js
+++ src/agents/emailAgent.js
@@ -2,7 +2,7 @@ import { Agent } from '@openai/agents';
 import { getUserSession } from '../tools/getConversation.js';
 import { vectorStoreSearchTool } from '../tools/vectorStoreSearch.js';
 import { isAppointmentDateAvailableTool, bookAppointmentTool, rejectAppointmentTool } from '../tools/appointmentTool.js';
-import { dateTimeFormaterAgent } from './dateTimeFormaterAgent.js';
+// import { dateTimeFormaterAgent } from './dateTimeFormaterAgent.js';
 
 export const emailAgent = new Agent({
     name: 'EmailAgent',
@@ -27,12 +27,11 @@ export const emailAgent = new Agent({
     If the user wants an appointment, ask for necessary details.
     If there is enough information to book an appointment and the user wants to book an appointment, 
     check if the appointment date is available using the isAppointmentDateAvailableTool and if it is available, then book the appointment using the bookAppointmentTool.
-    Convert the appointment date and time to the ISO format before calling the isAppointmentDateAvailableTool and bookAppointmentTool using the dateTimeFormaterAgent.
     Convert the appointment date and time to the ISO format before calling the isAppointmentDateAvailableTool and bookAppointmentTool.
     If the appointment date is not available, then inform the user that the appointment date is not available and ask for another date.
     If the user mentions that he does not want to book an appointment, then only update the appointment status to "REJECTED" using the rejectAppointmentTool.
     `,
-    tools: [getUserSession, vectorStoreSearchTool, dateTimeFormaterAgent, isAppointmentDateAvailableTool, bookAppointmentTool, rejectAppointmentTool],
+    tools: [getUserSession, vectorStoreSearchTool, isAppointmentDateAvailableTool, bookAppointmentTool, rejectAppointmentTool],
 });
 
 emailAgent.on('agent_start', (ctx, agent) => {
@@ -46,4 +45,5 @@ emailAgent.on('agent_end', (ctx, output) => {
 // You should then use the sendEmail tool to send a response to the user.
 // Then you should update the session with the new conversation history using the saveUserSession tool.
 // If the conversation is not related to appointment and just in the enquiry stage, then do not update the appointment status.
-// 
+//
+// Convert the appointment date and time to the ISO format before calling the isAppointmentDateAvailableTool and bookAppointmentTool using the dateTimeFormaterAgent.
